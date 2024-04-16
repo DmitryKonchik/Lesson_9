b58d927f1adb85a48e6d8b7a69d1487fef3a50d8
Dmitry <dkonchik@gmail.com>
2024-04-16 22:32:19 +0300

    Hard Task

---
+++ src/ClassToClone.java
@@ -0,0 +1,35 @@
+public class ClassToClone implements Cloneable{
+
+    private int age;
+    private String name;
+
+    public ClassToClone(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public ClassToClone clone() {
+        try {
+            return (ClassToClone) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-
+// First task
         Director director = new Director();
         Worker worker = new Worker();
         Accountant accountant = new Accountant();
         director.printPosition();
         worker.printPosition();
         accountant.printPosition();
-
+//  Second task
         Figure[] figures = {
                 new Triangle(3, 3, 5),
-                new Circle(7),
                 new Triangle(9, 3, 7),
+                new Circle(7),
                 new Rectangle(3, 7),
-                new Circle(10),
+                new Circle(10)
         };
 
         double sum = 0;
@@ -21,5 +23,13 @@ public class Main {
             sum += fig.perimeter();
         }
         System.out.println("\n" + sum);
+// Hard task
+        ClassToClone cl1 = new ClassToClone(20, "Bob");
+        ClassToClone cl2 = cl1.clone();
+        System.out.println(cl1.getAge() == cl2.getAge());
+        System.out.println(cl1.getName().equals(cl2.getName()));
+        cl2.setAge(30);
+        System.out.println(cl1.getAge() == cl2.getAge());
+
     }
 }
